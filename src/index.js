57c282fc7c04ac4a6ebec2cd588e268de2505ec5
reaganforonda <foronda.reagan@gmail.com>
2018-10-18 10:02:42 -0600

    removed css and added global reset styled-component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createGlobalStyle} from 'styled-components';
+import {reset} from './assets/reset';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalReset = createGlobalStyle`
+    ${reset}
+`
+
+ReactDOM.render(<React.Fragment><App /><GlobalReset/></React.Fragment>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
